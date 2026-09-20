2254d1161fd4b3e0b75f5faad211e44db2e8a718
deepthikrishna123 <deepthikrishna123@gmail.com>
2026-09-20 16:04:11 +0530

    assertionTask

--- tests/dropdownbox.spec.js
+++ tests/dropdownbox.spec.js
@@ -1,7 +1,20 @@
-import test from '@playwright/test'
+import {test,expect} from '@playwright/test'
 test("dropdownbox",async({page})=>{
     await page.goto("https://selenium.qabible.in/select-input.php")
     const dropdownbox=page.getByRole("combobox",{name:"Select Color"})
     await dropdownbox.selectOption("Red")
+    const selectColor=page.locator(".my-2").first()
+    await expect(selectColor).toBeVisible()
+    const msg=await selectColor.textContent()
+    console.log(msg)
+    expect(msg).toContain("Red")
+
+
+
+
+
+
+    await page.waitForTimeout(3000)
+
 
 })
